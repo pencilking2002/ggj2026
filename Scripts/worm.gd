9b04a64cdc37e7aba9ab16b1a7be45aa0a62d084
pencilking2002 <pencilking2002@gmail.com>
2026-01-28 16:58:52 -0500

    Starts adding worm behavior, organizes project, fixes UI scaling

--- Scripts/worm.gd
+++ Scripts/worm.gd
@@ -2,28 +2,39 @@ extends Node2D
 class_name Worm
 
 @export var sprite: Sprite2D
+@export var worm_settings:WormSettings
+
+var last_dive_time: float
+var last_emerge_time: float
+var time_elapsed: float
 
 enum WormStateEnum {
-	GOING_IN,
-	GOING_OUT,
-	IN_GROUND,
 	IDLE,
+	DIVING,
+	EMERGING,
+	SUBMERGED,
 }
 
 @export var worm_state: WormStateEnum = WormStateEnum.IDLE
 
 func _ready():
-	set_state(WormStateEnum.IDLE)
-	go_into_ground()
 	pass
 	
 func set_state(input_state: WormStateEnum):
 	worm_state = input_state
 	
-	
-func go_into_ground():
-	set_state(WormStateEnum.IN_GROUND)
-	pass
+func _process(delta: float):
+	time_elapsed += delta
 
-
-
+	if worm_state == WormStateEnum.SUBMERGED && Time.get_ticks_msec() > last_dive_time + worm_settings.submerged_duration:
+		set_state(WormStateEnum.EMERGING)
+		last_emerge_time = Time.get_ticks_msec()
+	elif worm_state == WormStateEnum.IDLE and Time.get_ticks_msec() > last_emerge_time + worm_settings.idle_duration:
+		set_state(WormStateEnum.DIVING)
+		last_dive_time = Time.get_ticks_msec()
+	elif worm_state == WormStateEnum.DIVING && Time.get_ticks_msec() > last_dive_time + worm_settings.dive_duration:
+		set_state(WormStateEnum.SUBMERGED)
+		last_dive_time = Time.get_ticks_msec()
+	elif worm_state == WormStateEnum.EMERGING && Time.get_ticks_msec() > last_emerge_time + worm_settings.emerge_duration:
+		set_state(WormStateEnum.IDLE)
+		last_emerge_time = Time.get_ticks_msec()
---
+++ Scripts/worm_settings.gd
@@ -0,0 +1,7 @@
+extends Resource
+class_name WormSettings
+
+@export var idle_duration := 6.0
+@export var submerged_duration := 2.0
+@export var dive_duration := 1.0
+@export var emerge_duration := 1.0
