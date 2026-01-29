bde84a38fe2640092c546577bf8be194f84ddb99
pencilking2002 <pencilking2002@gmail.com>
2026-01-29 11:59:44 -0500

    Adds ramp up to breathing

--- Scripts/mask_canvas_layer.gd
+++ Scripts/mask_canvas_layer.gd
@@ -5,6 +5,8 @@ class_name MaskCanvasLayer
 @export var timer: Timer
 @export var eye_radius_reduce_rate := 8.0
 @export var eye_falloff_reduce_rate := 2.0
+@export var fade_in_eye_radius_duration := 1.0
+@export var breath_audio_fade_in_duration := 10.0
 
 static var eye_radius_str: String = "eye_radius"
 static var eye_falloff_str: String = "eye_falloff"
@@ -14,13 +16,21 @@ var color_rect_mat: Material
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	color_rect_mat = color_rect.material
-	SoundManager.play_sound_mask()
+	fade_in_eye_radius(fade_in_eye_radius_duration)
+	SoundManager.play_sound_mask_with_ramp_up(breath_audio_fade_in_duration)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	# reduce_eye_radius(delta)
 	pass
 
+func fade_in_eye_radius(duration: float) -> void:
+	var tween: Tween = create_tween()
+	tween.tween_method(set_eye_radius, 0.0, 0.12, duration).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
+
+func set_eye_radius(value: float) -> void:
+	color_rect_mat.set_shader_parameter(eye_radius_str, value)
+	
 # Continually reduces the eye radius
 #func reduce_eye_radius(delta: float) -> void:
 #	var curr_radius: float = color_rect_mat.get_shader_parameter(eye_radius_str)
--- Scripts/sound_manager.gd
+++ Scripts/sound_manager.gd
@@ -4,3 +4,8 @@ extends Control
 
 func play_sound_mask():
 	$SoundMask.play()
+
+func play_sound_mask_with_ramp_up(volume_ramp_up_duration:float):
+	var tween: Tween = create_tween()
+	tween.tween_property($SoundMask, "volume_db", linear_to_db(0.5), volume_ramp_up_duration).from(linear_to_db(0.001))
+	$SoundMask.play()
