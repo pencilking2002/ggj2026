155b88dc53c3988e96630494c5962b603ca1437d
RollLikeABuffalo <egearrea@gmail.com>
2026-02-01 11:51:36 -0500

    begin mission complete screen setup

---
+++ _scenes/mission_complete.gd
@@ -0,0 +1,25 @@
+extends CanvasLayer
+
+@export var level : int
+@export var fade_in_duration := 1.0
+
+func _input(event):
+	if event is InputEventKey:
+		if event.pressed:
+			start_level()
+	elif event is InputEventMouseButton:
+		if event.pressed:
+			start_level()
+
+func start_level() -> void:
+	SoundManager.play_sound_geiger_single()
+	if level == 1 : 
+		get_tree().change_scene_to_file("res://_scenes/main.tscn")
+	if level == 2 : 
+		get_tree().change_scene_to_file("res://_scenes/main.tscn")
+	if level == 3 : 
+		get_tree().change_scene_to_file("res://_scenes/main.tscn")
+	if level == 4 : 
+		get_tree().change_scene_to_file("res://_scenes/main.tscn")
+	if level == 5 : 
+		get_tree().change_scene_to_file("res://_scenes/main.tscn")
