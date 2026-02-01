31668acf99534f9fc3468b29d9862f7070dd67eb
RollLikeABuffalo <egearrea@gmail.com>
2026-02-01 14:24:33 -0500

    added new sfx to manager

--- _scripts/sound_manager.gd
+++ _scripts/sound_manager.gd
@@ -10,6 +10,30 @@ func play_sound_mask_with_ramp_up(volume_ramp_up_duration:float):
 	tween.tween_property($SoundMask, "volume_db", linear_to_db(0.25), volume_ramp_up_duration).from(linear_to_db(0.001))
 	$SoundMask.play()
 
+func play_sound_pills():
+	$Pills.play()
+
+func stop_sound_pills():
+	$Pills.stop()
+	
+func play_sound_radiation():
+	$Radiationtouch.play()
+
+func stop_sound_radiation():
+	$Radiationtouch.stop()
+	
+func play_sound_worm_scream():
+	$Wormscream.play()
+
+func stop_sound_worm_scream():
+	$Wormscream.stop()
+	
+
+
+
+
+
+
 func play_sound_geiger_single(max_variance : float = 0.0):
 	$GeigerSingle.pitch_scale = 1.0 + randf() * max_variance
 	$GeigerSingle.play()
